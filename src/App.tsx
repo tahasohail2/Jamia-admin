2f20afdd66d37f6c5fce5945dd8d2b0b764abb69
Hamza Faheem <hamza.faheem@zekiexperts.com>
2026-03-18 00:45:37 +0500

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ToastProvider } from './context/ToastContext';
 import LoginPage from './pages/LoginPage';
